0503093eb9dfc0c5bd8999d5bffb0d2d03e41321
Daniel Castro <dancassig@gmail.com>
2016-06-20 01:04:02 -0500

    Adds basic chat

--- dchannels/consumers.py
+++ dchannels/consumers.py
@@ -1,10 +1,20 @@
 from channels.handler import AsgiHandler
 from django.http import HttpResponse
+from channels import Group
 
 
-def http_consumer(message):
-    # Make standard HTTP response - access ASGI path attribute directly
-    response = HttpResponse("Hello world! You asked for %s" % message.content['path'])
-    # Encode that response into message format (ASGI)
-    for chunk in AsgiHandler.encode_response(response):
-        message.reply_channel.send(chunk)
+# Connected to websocket.receive
+def ws_message(message):
+    Group("chat").send({
+        "text": "[user] %s" % message.content['text'],
+    })
+
+
+# Connected to websocket.connect
+def ws_add(message):
+    Group("chat").add(message.reply_channel)
+
+
+# Connected to websocket.disconnect
+def ws_disconnect(message):
+    Group("chat").discard(message.reply_channel)
--- websocket/routing.py
+++ websocket/routing.py
@@ -1,6 +1,8 @@
 from channels.routing import route
-
+from dchannels.consumers import ws_add, ws_message, ws_disconnect
 
 channel_routing = [
-    route("http.request", "dchannels.consumers.http_consumer"),
+    route("websocket.connect", ws_add),
+    route("websocket.receive", ws_message),
+    route("websocket.disconnect", ws_disconnect),
 ]
